4523405f187d7e1fb4354b2a74a97da1182843bb
mariaadradosr <mariaadradosr@gmail.com>
2020-06-19 10:11:26 +0200

    primer commit

---
+++ params.py
@@ -0,0 +1,15 @@
+
+best_params = {'n_estimators': 944,
+                'min_samples_split': 2,
+                'min_samples_leaf': 1,
+                'max_features': 'auto',
+                'max_depth': None,
+                'criterion': 'entropy',
+                'bootstrap': False}
+
+
+
+{'max_depth': 25,
+ 'min_samples_leaf': 1,
+ 'min_samples_split': 2,
+ 'n_estimators': 300}
